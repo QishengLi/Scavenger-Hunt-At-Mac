9f11fd6908b5206f4b28c8922ab71b2f271e177c
hys1435 <zli@macalester.edu>
2017-02-23 14:31:58 -0600

    Make map move with pikachu

--- core/src/com/mygdx/game/testMap.java
+++ core/src/com/mygdx/game/testMap.java
@@ -45,41 +45,42 @@ public class testMap extends ApplicationAdapter implements InputProcessor {
     }
 
     @Override public void render () {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1); // set background color
+        // Don't know what this line does - seems to work without it
         Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        camera.update();
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // Show the boundary of the map
+        camera.update(); // update the position of camera
         tiledMapRenderer.setView(camera);
-        tiledMapRenderer.render();
-        sb.setProjectionMatrix(camera.combined);
+        tiledMapRenderer.render(); // draw the map on canvas combined with the previous line
+        sb.setProjectionMatrix(camera.combined); // Combine the character with the camera?
         sb.begin();
-        player.draw(sb);
+        player.draw(sb); // draw the character
         sb.end();
     }
 
     // Called when a key was pressed
     @Override public boolean keyDown(int keycode) {
-        if(keycode == Input.Keys.LEFT)
+        if(keycode == Input.Keys.LEFT) {
             player.translateX(-32);
-        if(keycode == Input.Keys.RIGHT)
+            camera.translate(-32, 0);
+        }
+        if(keycode == Input.Keys.RIGHT) {
             player.translateX(32);
-        if(keycode == Input.Keys.UP)
+            camera.translate(32, 0);
+        }
+        if(keycode == Input.Keys.UP) {
             player.translateY(32);
-        if(keycode == Input.Keys.DOWN)
+            camera.translate(0, 32);
+        }
+        if(keycode == Input.Keys.DOWN) {
             player.translateY(-32);
+            camera.translate(0, -32);
+        }
         return false;
     }
 
     // Called when a key was released
     @Override public boolean keyUp(int keycode) {
-        if(keycode == Input.Keys.LEFT)
-            camera.translate(-32,0);
-        if(keycode == Input.Keys.RIGHT)
-            camera.translate(32,0);
-        if(keycode == Input.Keys.UP)
-            camera.translate(0,32);
-        if(keycode == Input.Keys.DOWN)
-            camera.translate(0,-32);
         if(keycode == Input.Keys.NUM_1)
             tiledMap.getLayers().get(0).setVisible(!tiledMap.getLayers().get(0).isVisible());
         if(keycode == Input.Keys.NUM_2)
@@ -91,6 +92,7 @@ public class testMap extends ApplicationAdapter implements InputProcessor {
         return false;
     }
 
+    // called when mouse was clicked
     @Override public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         Vector3 clickCoordinates = new Vector3(screenX,screenY,0);
         Vector3 position = camera.unproject(clickCoordinates);
