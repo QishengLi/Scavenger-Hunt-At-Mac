149de56985d1c01e170c87e2018dda62de696732
hys1435 <zli@macalester.edu>
2017-04-06 17:06:12 -0500

    fix healthbar bug

--- core/src/com/mygdx/game/entities/HealthBar.java
+++ core/src/com/mygdx/game/entities/HealthBar.java
@@ -16,6 +16,8 @@ public class HealthBar extends Group {
     private Image healthBarImg;
     private Label lifeLabel;
     private CampusMap map;
+    private final float HORIZONTAL_MARGIN = 250;
+    private final float VERTICAL_MARGIN = 70;
 
     public HealthBar(Texture bar, Texture healthBar, Label life_label, CampusMap map) {
         this.barImg = new Image(bar);
@@ -38,12 +40,18 @@ public class HealthBar extends Group {
         lifeLabel.setText("Life: "+Player.health);
         healthBarImg.setWidth(177*Player.health/Player.TOTALHEALTH);
         healthBarImg.setHeight(21);
-        this.setPosition(player.getX()+camera.viewportWidth/2-250, player.getY()+camera.viewportHeight/2-70);
+        this.setPosition(player.getX()+camera.viewportWidth/2-HORIZONTAL_MARGIN,
+                player.getY()+camera.viewportHeight/2-VERTICAL_MARGIN);
         this.setOrigin(barImg.getWidth()/2,barImg.getHeight()/2);
     }
 
+    //TODO: Refactor
     public void adjustBoundary(Player player, OrthographicCamera camera) {
 
+        boolean outOfBound = false;
+
+        float mapLeft = 0;
+        float mapBottom = 0;
         float mapWidth = map.mapWidth;
         float mapHeight = map.mapHeight;
 
@@ -51,17 +59,30 @@ public class HealthBar extends Group {
         float cameraHalfWidth = camera.viewportWidth * .5f;
         float cameraHalfHeight = camera.viewportHeight * .5f;
 
+        float cameraLeft = camera.position.x - cameraHalfWidth;
         float cameraRight = camera.position.x + cameraHalfWidth;
+        float cameraBottom = camera.position.y - cameraHalfHeight;
         float cameraTop = camera.position.y + cameraHalfHeight;
 
         if (cameraRight + Player.SPEED >= mapWidth) {
-            this.setX(mapWidth - 250);
+            this.setX(mapWidth - HORIZONTAL_MARGIN);
+            outOfBound = true;
         }
         if (cameraTop + Player.SPEED >= mapHeight) {
-            this.setY(mapHeight - 70);
+            this.setY(mapHeight - VERTICAL_MARGIN);
+            outOfBound = true;
         }
-        if (cameraRight + Player.SPEED < mapWidth && cameraTop + Player.SPEED < mapHeight) {
-            this.setPosition(player.getX()+camera.viewportWidth/2-250, player.getY()+camera.viewportHeight/2-70);
+        if (cameraLeft - Player.SPEED <= mapLeft) {
+            this.setX(cameraHalfWidth * 2 - HORIZONTAL_MARGIN);
+            outOfBound = true;
+        }
+        if (cameraBottom - Player.SPEED <= mapBottom) {
+            this.setY(cameraHalfHeight * 2 - VERTICAL_MARGIN);
+            outOfBound = true;
+        }
+        if (!outOfBound) {
+            this.setPosition(player.getX()+camera.viewportWidth/2-HORIZONTAL_MARGIN,
+                    player.getY()+camera.viewportHeight/2-VERTICAL_MARGIN);
         }
     }
 }
